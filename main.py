84bc43e4d20e064ee15867c974c55ae1ca4194e0
Raviteja Sagiraju <ravi.sagiraju@gmail.com>
2017-11-25 02:25:36 -0500

    OutageInfo

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import requests
-response = requests.get('https://httpbin.org/ip')
-print('Your IP is {0}'.format(response.json()['origin']))
+import requests, json
+
+url = 'http://outagemap.dominionenergy.com.s3.amazonaws.com/resources/data/external/interval_generation_data/2017_11_25_07_15_30/report_region.json'
+response = requests.get(url)
+
+parsed = json.loads(response.text)
+print(parsed['file_data']['areas'][0]['cust_a']['val'])
